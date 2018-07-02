7e98097a96c2f5dc10a86b1e9ff6214a94c99a35
William Schmitt <william@wschmitt.com>
2018-07-01 19:07:36 -0700

    Broken page up into components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContentWrapperComponent,
+    ContentComponent,
+    HeaderComponent,
+    StatusBarComponent
   ],
   imports: [
     BrowserModule
