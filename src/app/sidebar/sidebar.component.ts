37fe25ef6804e0fd72e7bf11f6f4e90c99fc1402
William Schmitt <william@wschmitt.com>
2018-07-05 15:29:49 -0700

    Add linking to sidebar.
    Adjusts header icon to match icons.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+  links = [
+  ];
+}
 
-  constructor() { }
+class Link {
+  href: string;
+  icon: string;
+  text: string;
 
-  ngOnInit() {
+  constructor(href: string, icon: string, text: string) {
+    this.href = href;
+    this.icon = icon;
+    this.text = text;
   }
-
 }
